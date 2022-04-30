5eeb770097148c40fbedc79d0649573679260deb
MDz1985 <m_d@tut.by>
2022-05-01 00:29:18 +0400

    feat: task-component added

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TaskComponent } from './components/task/task.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TaskComponent
+  ],
   imports: [
     CommonModule,
+    SharedModule
   ],
 })
 export class BoardModule { }
