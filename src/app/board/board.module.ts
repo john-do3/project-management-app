b242f7d4d22450e9af6944ce698778b4f7c35e07
MDz1985 <m_d@tut.by>
2022-05-06 03:16:58 +0400

    feat: column-component added

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskComponent } from './components/task/task.component';
 import { SharedModule } from '../shared/shared.module';
-// import { ColumnComponent } from './components/column/column.component';
+import { ColumnComponent } from './components/column/column.component';
 // import { BoardComponent } from './pages/board/board.component';
 
 @NgModule({
   declarations: [
     TaskComponent,
-    // ColumnComponent,
+    ColumnComponent,
     // BoardComponent,
   ],
   imports: [
