2a357bf27f88a3a72a6faf385ba491a38f1f916d
AlexAbramoff <alex93178@gmail.com>
2022-05-09 16:53:08 +0300

    feat: add welcome-page content

--- src/app/core/pages/welcome-page/welcome-page.component.ts
+++ src/app/core/pages/welcome-page/welcome-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { mainRoute, welcomeRoute } from 'src/app/project.constants';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.scss']
+  styleUrls: ['./welcome-page.component.scss'],
 })
 export class WelcomePageComponent implements OnInit {
+  isLoggedIn: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router, private userSrv: UserService) {}
 
-  ngOnInit(): void {
+  onLogin(): void {
+    this.router.navigateByUrl('auth/login');
   }
 
+  onSignUp(): void {
+    this.router.navigateByUrl('auth/signup');
+  }
+  ngOnInit(): void {
+    this.isLoggedIn = this.userSrv.checkIsLoggedIn();
+    if (this.isLoggedIn) this.router.navigateByUrl(mainRoute);
+  }
 }
